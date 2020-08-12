d84cab3c0f6c3e8878c95b47400167ca990ec073
Joshua Benuck <joshua_benuck@yahoo.com>
2020-08-11 18:36:38 -0700

    add ex command history

--- src/ex.rs
+++ src/ex.rs
@@ -20,6 +20,8 @@ pub struct Ex {
     buffer: String,
     pub result: String,
     cursor_pos: u16,
+    history: Vec<String>,
+    hindex: Option<usize>,
 }
 
 impl Ex {
@@ -29,6 +31,8 @@ impl Ex {
             buffer: "".to_string(),
             result: "".to_string(),
             cursor_pos: 0,
+            history: Vec::new(),
+            hindex: None,
         }
     }
 
@@ -49,7 +53,11 @@ impl Ex {
             .queue(cursor::MoveTo(0, row))?
             .queue(Clear(ClearType::CurrentLine))?;
         if self.active {
-            write!(stdout, ":{}", self.buffer)?;
+            if let Some(hindex) = self.hindex {
+                write!(stdout, ":{}", self.history[hindex])?;
+            } else {
+                write!(stdout, ":{}", self.buffer)?;
+            }
             stdout.queue(cursor::MoveTo(self.cursor_pos + 1, row))?;
         } else {
             write!(stdout, "{}", self.result)?;
@@ -73,10 +81,21 @@ impl Ex {
                 self.active = false;
             }
             KeyCode::Enter => {
-                self.active = false;
-                let command = std::mem::replace(&mut self.buffer, "".to_string());
-                self.cursor_pos = 0;
-                return ExEventStatus::Run(command);
+                let command = match self.hindex {
+                    None => std::mem::replace(&mut self.buffer, "".to_string()),
+                    Some(hindex) => {
+                        self.hindex = None;
+                        self.history.remove(hindex)
+                    }
+                };
+                if command.len() > 0 {
+                    self.active = false;
+                    self.cursor_pos = 0;
+                    if self.history.len() == 0 || self.history[self.history.len() - 1] != command {
+                        self.history.push(command.clone());
+                    }
+                    return ExEventStatus::Run(command);
+                }
             }
             KeyCode::Home => {
                 self.cursor_pos = 0;
@@ -84,13 +103,44 @@ impl Ex {
             KeyCode::End => {
                 self.cursor_pos = max(self.buffer.len() as i16 - 1, 0) as u16;
             }
+            KeyCode::Up if self.buffer.len() == 0 => match self.hindex {
+                Some(hindex) => {
+                    self.hindex = Some(max(hindex as isize - 1, 0) as usize);
+                    self.cursor_pos = self.history[self.hindex.unwrap()].len() as u16;
+                }
+                None if self.history.len() > 0 => {
+                    self.hindex = Some(self.history.len() - 1);
+                    self.cursor_pos = self.history[self.hindex.unwrap()].len() as u16;
+                }
+                _ => {}
+            },
+            KeyCode::Down => match self.hindex {
+                Some(hindex) => {
+                    let next_index = hindex + 1;
+                    if next_index >= self.history.len() {
+                        self.hindex = None;
+                        self.cursor_pos = 0;
+                    } else {
+                        self.hindex = Some(next_index);
+                        self.cursor_pos = self.history[self.hindex.unwrap()].len() as u16;
+                    }
+                }
+                _ => {}
+            },
             KeyCode::Left => {
                 self.cursor_pos = max(self.cursor_pos as i16 - 1, 0) as u16;
             }
-            KeyCode::Right => {
-                self.cursor_pos = min(self.cursor_pos + 1, self.buffer.len() as u16);
-            }
+            KeyCode::Right => match self.hindex {
+                Some(hindex) => {
+                    self.cursor_pos = min(self.cursor_pos + 1, self.history[hindex].len() as u16)
+                }
+                None => self.cursor_pos = min(self.cursor_pos + 1, self.buffer.len() as u16),
+            },
             KeyCode::Backspace => {
+                if let Some(hindex) = self.hindex {
+                    self.buffer = self.history[hindex].clone();
+                    self.hindex = None;
+                }
                 if self.buffer.len() > 0 {
                     let new_cursor_pos = max(self.cursor_pos as i16 - 1, 0) as u16;
                     let before = &self.buffer[0..new_cursor_pos as usize];
@@ -102,6 +152,10 @@ impl Ex {
                 }
             }
             KeyCode::Char(c) => {
+                if let Some(hindex) = self.hindex {
+                    self.buffer = self.history[hindex].clone();
+                    self.hindex = None;
+                }
                 self.buffer.insert(self.cursor_pos as usize, c);
                 self.cursor_pos += 1;
             }
